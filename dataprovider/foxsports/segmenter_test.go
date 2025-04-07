bedaa584510c935e7d74b6f55c7f3bb8ee38a2df
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2025-04-07 02:26:06 -0400

    unit tests for GeneralSegmenter and NFLSegmenter

--- dataprovider/foxsports/segmenter_test.go
+++ dataprovider/foxsports/segmenter_test.go
@@ -1,3 +1,72 @@
+//go:build unit
+
 package foxsports
 
-// TODO: Write unit tests for GeneralSementer and NFLSementer
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGeneralSegmenter(t *testing.T) {
+	tests := []struct {
+		name     string
+		date     string
+		isError  bool
+		expected string
+	}{
+		{
+			name:     "valid date",
+			date:     "2025-04-07",
+			expected: "20250407",
+		},
+		{
+			name:    "exception",
+			date:    "2025-4-7",
+			isError: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			segmenter := GeneralSegmenter{Date: tt.date}
+			actual, err := segmenter.GetSegmentId()
+			if tt.isError {
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.expected, actual)
+			}
+		})
+	}
+}
+
+func TestNFLSegmenter(t *testing.T) {
+	tests := []struct {
+		name     string
+		year     int32
+		week     int32
+		season   SeasonType
+		expected string
+	}{
+		{
+			name:     "regular season",
+			year:     2024,
+			week:     10,
+			season:   REGULARSEASON,
+			expected: "2024-10-1",
+		},
+		{
+			name:     "post season",
+			year:     2024,
+			week:     2,
+			season:   POSTSEASON,
+			expected: "2024-2-2",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			segmenter := NFLSegmenter{Year: tt.year, Week: tt.week, Season: tt.season}
+			actual, _ := segmenter.GetSegmentId()
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
