42f64e656cba9c1eac6ede9ea3fb5834809a97d5
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2025-04-08 02:53:36 -0400

    v0.5.0-alpha.1

--- version/version.go
+++ version/version.go
@@ -2,4 +2,4 @@ package version
 
 // Version is a manually maintained semver version.
 // This is updated by hand when releasing new versions
-const Version = "v0.5.0-alpha"
+const Version = "v0.5.0-alpha.1"
