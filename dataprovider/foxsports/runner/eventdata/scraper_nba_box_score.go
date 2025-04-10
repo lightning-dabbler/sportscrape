a0f16f136ba36c3bf0d6e1c7049eda03792b7292
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2025-04-10 02:31:36 -0400

    validation of headers and parseBoxScoreStats init

--- dataprovider/foxsports/runner/eventdata/scraper_nba_box_score.go
+++ dataprovider/foxsports/runner/eventdata/scraper_nba_box_score.go
@@ -2,10 +2,14 @@ package eventdata
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"strings"
+	"time"
 
 	"github.com/lightning-dabbler/sportscrape/dataprovider/foxsports/jsonresponse"
 	"github.com/lightning-dabbler/sportscrape/dataprovider/foxsports/model"
+	"github.com/lightning-dabbler/sportscrape/util"
 )
 
 var NBABoxscoreStatsHeaders map[string][]string = map[string][]string{
@@ -19,6 +23,7 @@ type NBABoxScoreScraper struct {
 }
 
 func (s *NBABoxScoreScraper) Scrape(matchup interface{}) OutputWrapper {
+	start := time.Now().UTC()
 	matchupModel := matchup.(model.Matchup)
 	var context Context
 	context.AwayTeam = matchupModel.AwayTeamNameFull
@@ -63,19 +68,129 @@ func (s *NBABoxScoreScraper) Scrape(matchup interface{}) OutputWrapper {
 	}
 
 	// validate NBABoxScoreStats home and away positions
+	uriSplit := strings.Split(responsePayload.BoxScore.BoxScoreSections.HomePlayerStats.ContentURI, "/")
+	actualHomeID, err := util.TextToInt64(uriSplit[len(uriSplit)-1])
+	if actualHomeID != matchupModel.HomeTeamID {
+		log.Printf("Home team ID, %d (%s), does not match expected, %d (%s)\n", actualHomeID, responsePayload.BoxScore.BoxScoreSections.HomePlayerStats.Title, matchupModel.HomeTeamID, matchupModel.HomeTeamNameFull)
+		return OutputWrapper{Error: err, Context: context}
+	}
 
-	// validate home starter headers index 0
-	// validate home bench headers index 1
-	// validate home shooting headers index 3
+	uriSplit = strings.Split(responsePayload.BoxScore.BoxScoreSections.AwayPlayerStats.ContentURI, "/")
+	actualAwayID, err := util.TextToInt64(uriSplit[len(uriSplit)-1])
+	if actualAwayID != matchupModel.AwayTeamID {
+		log.Printf("Away team ID, %d (%s), does not match expected, %d (%s)\n", actualAwayID, responsePayload.BoxScore.BoxScoreSections.AwayPlayerStats.Title, matchupModel.AwayTeamID, matchupModel.AwayTeamNameFull)
+		return OutputWrapper{Error: err, Context: context}
+	}
 
-	// validate away starter headers index 0
-	// validate away bench headers index 1
-	// validate away shooting headers index 3
+	// validate headers
+	expectedStartersHeaders := NBABoxscoreStatsHeaders["starters"]
+	expectedStarterHeaderSize := len(expectedStartersHeaders)
+	expectedBenchHeaders := NBABoxscoreStatsHeaders["bench"]
+	expectedBenchHeaderSize := len(expectedBenchHeaders)
+	expectedShootingHeaders := NBABoxscoreStatsHeaders["shooting"]
+	expectedShootingHeaderSize := len(expectedShootingHeaders)
 
-	// var playerMap map[int64]model.NBABoxScoreStats
-	// parse and allocate relevant data to playerMap
+	// validate home starter headers (index 0)
+	actualHeaders := responsePayload.BoxScore.BoxScoreSections.HomePlayerStats.BoxscoreItems[0].BoxscoreTable.Headers[0].Columns
+	actualHeaderSize := len(actualHeaders)
+	if actualHeaderSize != expectedStarterHeaderSize {
+		err = fmt.Errorf("Home team starter headers size mismatch. actual: %d expected: %d", actualHeaderSize, expectedStarterHeaderSize)
+		return OutputWrapper{Error: err, Context: context}
+	}
+	for idx, column := range actualHeaders {
+		if column.Text != expectedStartersHeaders[idx] {
+			err = fmt.Errorf("Home team starter header '%s' unexpect at index %d. Expected %s", column.Text, idx, expectedStartersHeaders[idx])
+			return OutputWrapper{Error: err, Context: context}
+		}
+	}
+
+	// validate home bench headers (index 1)
+	actualHeaders = responsePayload.BoxScore.BoxScoreSections.HomePlayerStats.BoxscoreItems[1].BoxscoreTable.Headers[0].Columns
+	actualHeaderSize = len(actualHeaders)
+	if actualHeaderSize != expectedBenchHeaderSize {
+		err = fmt.Errorf("Home team bench headers size mismatch. actual: %d expected: %d", actualHeaderSize, expectedBenchHeaderSize)
+		return OutputWrapper{Error: err, Context: context}
+	}
+	for idx, column := range actualHeaders {
+		if column.Text != expectedBenchHeaders[idx] {
+			err = fmt.Errorf("Home team bench header '%s' unexpect at index %d. Expected %s", column.Text, idx, expectedBenchHeaders[idx])
+			return OutputWrapper{Error: err, Context: context}
+		}
+	}
+
+	// validate home shooting headers (index 3)
+	actualHeaders = responsePayload.BoxScore.BoxScoreSections.HomePlayerStats.BoxscoreItems[3].BoxscoreTable.Headers[0].Columns
+	actualHeaderSize = len(actualHeaders)
+	if actualHeaderSize != expectedShootingHeaderSize {
+		err = fmt.Errorf("Home team shooting headers size mismatch. actual: %d expected: %d", actualHeaderSize, expectedShootingHeaderSize)
+		return OutputWrapper{Error: err, Context: context}
+	}
+	for idx, column := range actualHeaders {
+		if column.Text != expectedShootingHeaders[idx] {
+			err = fmt.Errorf("Home team shooting header '%s' unexpect at index %d. Expected %s", column.Text, idx, expectedShootingHeaders[idx])
+			return OutputWrapper{Error: err, Context: context}
+		}
+	}
+
+	// validate away starter headers (index 0)
+	actualHeaders = responsePayload.BoxScore.BoxScoreSections.AwayPlayerStats.BoxscoreItems[0].BoxscoreTable.Headers[0].Columns
+	actualHeaderSize = len(actualHeaders)
+	if actualHeaderSize != expectedStarterHeaderSize {
+		err = fmt.Errorf("Away team starter headers size mismatch. actual: %d expected: %d", actualHeaderSize, expectedStarterHeaderSize)
+		return OutputWrapper{Error: err, Context: context}
+	}
+	for idx, column := range actualHeaders {
+		if column.Text != expectedStartersHeaders[idx] {
+			err = fmt.Errorf("Away team starter header '%s' unexpect at index %d. Expected %s", column.Text, idx, expectedStartersHeaders[idx])
+			return OutputWrapper{Error: err, Context: context}
+		}
+	}
+
+	// validate away bench headers (index 1)
+	actualHeaders = responsePayload.BoxScore.BoxScoreSections.AwayPlayerStats.BoxscoreItems[1].BoxscoreTable.Headers[0].Columns
+	actualHeaderSize = len(actualHeaders)
+	if actualHeaderSize != expectedBenchHeaderSize {
+		err = fmt.Errorf("Away team bench headers size mismatch. actual: %d expected: %d", actualHeaderSize, expectedBenchHeaderSize)
+		return OutputWrapper{Error: err, Context: context}
+	}
+	for idx, column := range actualHeaders {
+		if column.Text != expectedBenchHeaders[idx] {
+			err = fmt.Errorf("Away team bench header '%s' unexpect at index %d. Expected %s", column.Text, idx, expectedBenchHeaders[idx])
+			return OutputWrapper{Error: err, Context: context}
+		}
+	}
+
+	// validate away shooting headers (index 3)
+	actualHeaders = responsePayload.BoxScore.BoxScoreSections.AwayPlayerStats.BoxscoreItems[3].BoxscoreTable.Headers[0].Columns
+	actualHeaderSize = len(actualHeaders)
+	if actualHeaderSize != expectedShootingHeaderSize {
+		err = fmt.Errorf("Away team shooting headers size mismatch. actual: %d expected: %d", actualHeaderSize, expectedShootingHeaderSize)
+		return OutputWrapper{Error: err, Context: context}
+	}
+	for idx, column := range actualHeaders {
+		if column.Text != expectedShootingHeaders[idx] {
+			err = fmt.Errorf("Away team shooting header '%s' unexpect at index %d. Expected %s", column.Text, idx, expectedShootingHeaders[idx])
+			return OutputWrapper{Error: err, Context: context}
+		}
+	}
+
+	// parse and develop playerMap of relevant statlines
+	playerMap, err := s.parseBoxScoreStats(responsePayload)
+	if err != nil {
+		return OutputWrapper{Error: err, Context: context}
+	}
 
 	// Allocate each statline to data output
-
+	for _, obj := range playerMap {
+		data = append(data, obj)
+	}
+	diff := time.Now().UTC().Sub(start)
+	log.Printf("Scraping of event %d (%s vs %s) completed in %s\n", matchupModel.EventID, matchupModel.AwayTeamNameFull, matchupModel.HomeTeamNameFull, diff)
 	return OutputWrapper{Output: data, Context: context}
+}
+
+func (s *NBABoxScoreScraper) parseBoxScoreStats(responsePayload jsonresponse.NBAEventData) (map[int64]model.NBABoxScoreStats, error) {
+	var playerMap map[int64]model.NBABoxScoreStats
+	return playerMap, nil
+
 }
