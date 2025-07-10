5ee8221722d0736f1f6238e67dc79f7b98d4f65e
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2025-07-10 01:40:52 -0400

    ExampleBattingBoxScoreScraper

--- dataprovider/baseballsavantmlb/example_test.go
+++ dataprovider/baseballsavantmlb/example_test.go
@@ -68,3 +68,39 @@ func ExampleFieldingBoxScoreScraper() {
 	}
 
 }
+
+// Example for baseballsavantmlb.BattingBoxScoreScraper
+func ExampleBattingBoxScoreScraper() {
+	matchupscraper := baseballsavantmlb.NewMatchupScraper(
+		baseballsavantmlb.MatchupScraperDate("2025-06-25"),
+	)
+	matchuprunner := sportscrape.NewMatchupRunner(
+		sportscrape.MatchupRunnerScraper(matchupscraper),
+	)
+	matchups, err := matchuprunner.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	boxscorescraper := baseballsavantmlb.NewBattingBoxScoreScraper()
+
+	boxscorerunner := sportscrape.NewEventDataRunner(
+		sportscrape.EventDataRunnerScraper(boxscorescraper),
+		sportscrape.EventDataRunnerConcurrency(1),
+	)
+
+	stats, err := boxscorerunner.Run(matchups...)
+	if err != nil {
+		panic(err)
+	}
+
+	// Output each statline as pretty json
+	for _, statline := range stats {
+		jsonBytes, err := json.MarshalIndent(statline, "", "  ")
+		if err != nil {
+			log.Fatalf("Error marshaling to JSON: %v\n", err)
+		}
+		fmt.Println(string(jsonBytes))
+	}
+
+}
