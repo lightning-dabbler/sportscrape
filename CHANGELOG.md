d093820cd38c16464c120177a164eb894cd94d60
Osarodion <33585132+lightning-dabbler@users.noreply.github.com>
2025-10-30 01:11:25 -0400

    Release/v0.16.2 (#111)
    * ESPN MMA: data model parquet type bug fix (#110)
    
    * correct parquet type for event_id
    
    * update to changelog
    
    * update to comment
    
    * pr number
    
    * v0.16.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.16.2] - 2025-10-30
+### Fixed
+- ESPN MMA: Fixed parquet type for event_id in matchups and fight details data models (#110)
+
 ## [0.16.1] - 2025-10-29
 ### Fixed
 - Error handling when retrieving document for ESPN MMA matchups (#108)
--- dataprovider/espn/mma/model/matchup.go
+++ dataprovider/espn/mma/model/matchup.go
@@ -10,8 +10,8 @@ type Matchup struct {
 	PullTimestamp time.Time `json:"pull_timestamp"`
 	// PullTimestampParquet is the fetch timestamp (in milliseconds)
 	PullTimestampParquet int64 `json:"-" parquet:"name=pull_timestamp, type=INT64, logicaltype=TIMESTAMP, logicaltype.unit=MILLIS, logicaltype.isadjustedtoutc=true, convertedtype=TIMESTAMP_MILLIS"`
-	// EventID is a unique ID that maps to the matchup e.g. 86833
-	EventID string `json:"event_id" parquet:"name=event_id, type=INT64"`
+	// EventID is a unique ID that maps to the matchup e.g. 600044733
+	EventID string `json:"event_id" parquet:"name=event_id, type=BYTE_ARRAY, convertedtype=UTF8"`
 	// EventTime is the timestamp associated with the matchup
 	EventTime time.Time `json:"event_time"`
 	// EventTimeParquet is the timestamp associated with the matchup (in milliseconds)
