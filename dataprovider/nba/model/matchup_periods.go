dc058c11845e2a18ef2547ac487ef318c2fa1b49
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2025-11-19 12:48:58 -0500

    update to comments

--- dataprovider/nba/model/matchup_periods.go
+++ dataprovider/nba/model/matchup_periods.go
@@ -8,7 +8,7 @@ type MatchupPeriods struct {
 	PullTimestamp time.Time `json:"pull_timestamp"`
 	// PullTimestampParquet is the fetch timestamp (in milliseconds)
 	PullTimestampParquet int64 `json:"-" parquet:"name=pull_timestamp, type=INT64, logicaltype=TIMESTAMP, logicaltype.unit=MILLIS, logicaltype.isadjustedtoutc=true, convertedtype=TIMESTAMP_MILLIS"`
-	// EventID is a unique ID that maps to the matchup e.g. 0022500249
+	// EventID is a string ID that maps to the matchup e.g. 0022500249
 	EventID string `json:"event_id" parquet:"name=event_id, type=BYTE_ARRAY, convertedtype=UTF8"`
 	// EventTime is the timestamp associated with the matchup
 	EventTime time.Time `json:"event_time"`
