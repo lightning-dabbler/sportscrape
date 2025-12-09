a60aba01bc73f2688a06d30ce8cdf277dd361c40
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2025-12-08 22:39:43 -0500

    play by play example

--- dataprovider/nba/example_test.go
+++ dataprovider/nba/example_test.go
@@ -57,3 +57,41 @@ func ExampleMatchupPeriodsScraper() {
 		fmt.Println(string(jsonBytes))
 	}
 }
+
+// Example for nba.PlayByPlayScraper
+func ExamplePlayByPlayScraper() {
+	matchupScraper := nba.NewMatchupScraper(
+		nba.WithMatchupDate("2025-06-05"),
+		nba.WithMatchupTimeout(2*time.Minute),
+	)
+	matchuprunner := runner.NewMatchupRunner(
+		runner.MatchupRunnerScraper(matchupScraper),
+	)
+
+	matchups, err := matchuprunner.Run()
+	if err != nil {
+		panic(err)
+	}
+
+	playbyplayscraper := nba.NewPlayByPlayScraper(
+		nba.WithPlayByPlayTimeout(2 * time.Minute),
+	)
+
+	playbyplayrunner := runner.NewEventDataRunner(
+		runner.EventDataRunnerScraper(playbyplayscraper),
+		runner.EventDataRunnerConcurrency(1),
+	)
+
+	records, err := playbyplayrunner.Run(matchups...)
+	if err != nil {
+		panic(err)
+	}
+	// Output each statline as pretty json
+	for _, period := range records {
+		jsonBytes, err := json.MarshalIndent(period, "", "  ")
+		if err != nil {
+			log.Fatalf("Error marshaling to JSON: %v\n", err)
+		}
+		fmt.Println(string(jsonBytes))
+	}
+}
