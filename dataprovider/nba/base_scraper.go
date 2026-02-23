ae5587dc40a552aed8649e6a240d68b128528f65
Osarodion Irabor <33585132+lightning-dabbler@users.noreply.github.com>
2026-02-22 23:25:33 -0500

    141

--- dataprovider/nba/base_scraper.go
+++ dataprovider/nba/base_scraper.go
@@ -19,7 +19,7 @@ type Scraper struct {
 func (s Scraper) FetchDoc(URL string) (string, error) {
 	doc, err := s.RetrieveDocument(URL, network.Headers{
 		"user-agent":       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36",
-		"sec-ch-ua":        `"Chromium";v="136", "Google Chrome";v="136", "Not.A/Brand";v="99"`,
+		"sec-ch-ua":        `"Chromium";v="141", "Google Chrome";v="141", "Not.A/Brand";v="99"`,
 		"sec-ch-ua-mobile": "?0",
 		"accept":           "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
 		"sec-fetch-dest":   "document",
